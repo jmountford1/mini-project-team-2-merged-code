ef88210d6f07818a54b402299af49a837ee94669
James Mountford <jimmy_mountford@yahoo.com>
2024-09-18 00:31:04 -0500

    remove accidental merged code

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <>
-      <div className="absolute bg-zinc-900 text-white p-4 w-screen text-center">
-        <footer> &copy; To-Do app, 2024</footer>
-      </div>
-    </>
-  );
-}
